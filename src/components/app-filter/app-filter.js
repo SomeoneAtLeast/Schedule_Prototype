9f4ac22ec14b593a0a7653db12ea2d065fc1037f
Вадим Бабичев <WineCooler@yandex.ru>
2021-01-16 21:20:18 +0300

    Настроен стейт кнопок фильтра

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -3,13 +3,37 @@ import React, {Component} from "react";
 import "./app-filter.css"
 
 export default class Filter extends Component {
+    constructor () {
+        super();
+
+        this.state = {
+            active: false,
+        };
+
+        this.onActive = this.onActive.bind(this);
+    }
+
+    onActive () {
+        this.setState(({active}) => ({
+            active: !active
+        }))
+    }
 
     render () {
         // eslint-disable-next-line react/prop-types
         const {btnText} = this.props;
+        const {active} = this.state;
+        let classNames = "filter-list__filter";
+
+        if (active) {
+            classNames += " active";
+        }
+
         return (
-            <li className="filter-list__filter">
-                <button className="filter-list__filter-btn">
+            <li className={classNames}>
+                <button
+                className="filter-list__filter-btn"
+                onClick = {this.onActive}>
                     {btnText}
                 </button>
             </li>
