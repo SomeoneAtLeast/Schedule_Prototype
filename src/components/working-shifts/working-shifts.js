aeca65bf44915f9532f8d96d223cbbac6f14fea7
Вадим Бабичев <WineCooler@yandex.ru>
2021-02-18 21:24:58 +0300

    Доработка редактирования текста смен

--- src/components/working-shifts/working-shifts.js
+++ src/components/working-shifts/working-shifts.js
@@ -55,27 +55,43 @@ emptyShifts.forEach(item => {
 
 const shifts = emptyShifts;
 
+let glArr = [
+    {
+        glName: "Групп-лидер 1",
+        teamName: "Команда 1",
+        shiftNumber: "Смена 1",
+        id: 1001
+    },
+    {
+        glName: "Групп-лидер 2",
+        teamName: "Команда 2",
+        shiftNumber: "Смена 2",
+        id: 1002
+    },
+    {
+        glName: "Групп-лидер 3",
+        teamName: "Команда 3",
+        shiftNumber: "Смена 1 и 2",
+        id: 1003
+    },
+    {
+        shiftName: "№ Смены",
+        firstShiftNumber: "1",
+        secondShiftNumber: "2",
+        id: 1004
+    },
+];
+
+const glTable = glArr;
+
 export default class WorkingShifts extends Component {
     constructor() {
         super();
 
         this.state = {
-            shifts: shifts
+            shifts: shifts,
+            glTable: glTable
         }
-    }
-
-    onChangeShiftsText(id, startTime, finishTime) {
-        console.log(id, startTime, finishTime);
-        // this.setState(({shifts}) => {
-        //     const index = shifts.findIndex(elem => elem.id === id); 
-        //     const oldShift = shifts[index];
-        //     const newShift = {...oldShift, startTime: startTime, finishTime: finishTime}
-        //     const newShifts = [...shifts.slice(0, index), newShift, ...shifts.slice(index + 1)];
-
-        //     return {
-        //         shifts: newShifts
-        //     }
-        // })
     }
 
     tableKmRows (number) {
@@ -88,38 +104,162 @@ export default class WorkingShifts extends Component {
         )
     }
 
-    table (startTime, finishTime, numberOfRows, shiftNumber, key) {
-        let rows = [];
-        
-        for (let i = 0; i <= numberOfRows; i++) {
-            rows.push(<WorkingShiftsSocialItem shifts={this.state.shifts[shiftNumber]} key={this.state.shifts[i].id}/>);
-        }
-
-        return (
-            <table className="working-shifts__table" key={key}>
-                <tbody>
-                    <tr className={`working-shifts__table-row-header working-shifts__table-row-${startTime}-${finishTime}`}>
-                        <th 
-                            className={`working-shifts__table-header working-shifts__table-header-${startTime}-${finishTime}`}
-                            colSpan="4"
-                            onClick={() => this.onChangeShiftsText(key, 1, 2)}>С {startTime} до {finishTime}</th>
-                    </tr>
-                    {rows}
-                </tbody>
-            </table>
-        )
-    }
-
-    tables (tablesNumber) {
-        let tablesArr = [];
-        for (let i = 0; i <= (tablesNumber - 1); i++) {
-            tablesArr.push(this.table(this.state.shifts[i].startTime, this.state.shifts[i].finishTime, getRandomNumber(0, 2), i, i))
-        }
-
-        return tablesArr
-    }
-
     render() {
+            const {shifts, glTable} = this.state;
+
+        const onStartTimeChange = (id) => (e) => {
+            console.log(id, e.target.value);
+
+            this.setState(({shifts}) => {
+                const index = shifts.findIndex(elem => elem.id === (id + 100)); 
+                const oldShift = shifts[index];
+                const newShift = {...oldShift, startTime: e.target.value}
+                const newShifts = [...shifts.slice(0, index), newShift, ...shifts.slice(index + 1)];
+    
+                return {
+                    shifts: newShifts
+                }
+            })
+        }
+
+        const onFinishTimeChange = (id) => (e) => {
+            console.log(id, e.target.value);
+
+            this.setState(({shifts}) => {
+                const index = shifts.findIndex(elem => elem.id === (id + 100)); 
+                console.log(id)
+                console.log(index)
+                const oldShift = shifts[index];
+                const newShift = {...oldShift, finishTime: e.target.value}
+                const newShifts = [...shifts.slice(0, index), newShift, ...shifts.slice(index + 1)];
+    
+                return {
+                    shifts: newShifts
+                }
+            })
+        }
+
+        const onGlNameChange = (id) => (e) => {
+            this.setState(({glTable}) => {
+                const index = glTable.findIndex(elem => elem.id === id); 
+                const oldGlTableItem = glTable[index];
+                const newGlTableItem = {...oldGlTableItem, glName: e.target.value}
+                const newGlTable = [...glTable.slice(0, index), newGlTableItem, ...glTable.slice(index + 1)];
+    
+                return {
+                    glTable: newGlTable
+                }
+            })
+        }
+
+        const onTeamNameChange = (id) => (e) => {
+            this.setState(({glTable}) => {
+                const index = glTable.findIndex(elem => elem.id === id); 
+                const oldGlTableItem = glTable[index];
+                const newGlTableItem = {...oldGlTableItem, teamName: e.target.value}
+                const newGlTable = [...glTable.slice(0, index), newGlTableItem, ...glTable.slice(index + 1)];
+    
+                return {
+                    glTable: newGlTable
+                }
+            })
+        }
+
+        const onShiftNumberChange = (id) => (e) => {
+            this.setState(({glTable}) => {
+                const index = glTable.findIndex(elem => elem.id === id); 
+                const oldGlTableItem = glTable[index];
+                const newGlTableItem = {...oldGlTableItem, shiftNumber: e.target.value}
+                const newGlTable = [...glTable.slice(0, index), newGlTableItem, ...glTable.slice(index + 1)];
+    
+                return {
+                    glTable: newGlTable
+                }
+            })
+        }
+
+        const onShiftNameChange = (id) => (e) => {
+            this.setState(({glTable}) => {
+                const index = glTable.findIndex(elem => elem.id === id); 
+                const oldGlTableItem = glTable[index];
+                const newGlTableItem = {...oldGlTableItem, shiftName: e.target.value}
+                const newGlTable = [...glTable.slice(0, index), newGlTableItem, ...glTable.slice(index + 1)];
+    
+                return {
+                    glTable: newGlTable
+                }
+            })
+        }
+
+        const onFirstShiftNumberChange = (id) => (e) => {
+            this.setState(({glTable}) => {
+                const index = glTable.findIndex(elem => elem.id === id); 
+                const oldGlTableItem = glTable[index];
+                const newGlTableItem = {...oldGlTableItem, firstShiftNumber: e.target.value}
+                const newGlTable = [...glTable.slice(0, index), newGlTableItem, ...glTable.slice(index + 1)];
+    
+                return {
+                    glTable: newGlTable
+                }
+            })
+        }
+
+        const onSecondShiftNumberChange = (id) => (e) => {
+            this.setState(({glTable}) => {
+                const index = glTable.findIndex(elem => elem.id === id); 
+                const oldGlTableItem = glTable[index];
+                const newGlTableItem = {...oldGlTableItem, secondShiftNumber: e.target.value}
+                const newGlTable = [...glTable.slice(0, index), newGlTableItem, ...glTable.slice(index + 1)];
+    
+                return {
+                    glTable: newGlTable
+                }
+            })
+        }
+
+        const table = (startTime, finishTime, numberOfRows, shiftNumber, key) => {
+            let rows = [];
+            
+            for (let i = 0; i <= numberOfRows; i++) {
+                rows.push(<WorkingShiftsSocialItem shifts={shifts[shiftNumber]} key={shifts[i].id}/>);
+            }
+    
+            return (
+                <table className="working-shifts__table" key={key}>
+                    <tbody>
+                        <tr className={`working-shifts__table-row-header working-shifts__table-row-${startTime}-${finishTime}`}>
+                            <th 
+                                className={`working-shifts__table-header working-shifts__table-header-${startTime}-${finishTime}`}
+                                colSpan="4"
+                                >
+                                    С
+                                    <input
+                                        className="working-shifts__table-header-value"
+                                        type="text"
+                                        value={startTime}
+                                        onChange={onStartTimeChange(key)}/>
+                                    до
+                                    <input
+                                        className="working-shifts__table-header-value"
+                                        type="text"
+                                        value={finishTime}
+                                        onChange={onFinishTimeChange(key)}/>
+                            </th>
+                        </tr>
+                        {rows}
+                    </tbody>
+                </table>
+            )
+        }
+
+        const tables = (tablesNumber) => {
+            let tablesArr = [];
+            for (let i = 0; i <= (tablesNumber - 1); i++) {
+                tablesArr.push(table(shifts[i].startTime, shifts[i].finishTime, 1, i, i))
+            }
+    
+            return tablesArr
+        }
 
         return (
             <div className="working-shifts">
@@ -133,26 +273,110 @@ export default class WorkingShifts extends Component {
                         </caption>
                         <tbody>
                             <tr className="working-shifts__table-row-header working-shifts__table-row-gl">
-                                <th className="working-shifts__table-header working-shifts__table-header-gl">Групп-лидер 1</th>
-                                <th className="working-shifts__table-header working-shifts__table-header-gl">Групп-лидер 2</th>
-                                <th className="working-shifts__table-header working-shifts__table-header-gl">Групп-лидер 3</th>
-                                <th className="working-shifts__table-header working-shifts__table-header-gl">№ Смены</th>
+                                <th className="working-shifts__table-header working-shifts__table-header-gl">
+                                    <input
+                                        className="working-shifts__table-header-value"
+                                        type="text"
+                                        onChange={onGlNameChange(1001)}
+                                        value={glTable[0].glName}
+                                        />
+                                    </th>
+                                <th className="working-shifts__table-header working-shifts__table-header-gl">
+                                    <input
+                                        className="working-shifts__table-header-value"
+                                        type="text"
+                                        onChange={onGlNameChange(1002)}
+                                        value={glTable[1].glName}
+                                        />
+                                </th>
+                                <th className="working-shifts__table-header working-shifts__table-header-gl">
+                                    <input
+                                        className="working-shifts__table-header-value"
+                                        type="text"
+                                        onChange={onGlNameChange(1003)}
+                                        value={glTable[2].glName}
+                                        />
+                                </th>
+                                <th className="working-shifts__table-header working-shifts__table-header-gl">
+                                    <input
+                                        className="working-shifts__table-header-value"
+                                        type="text"
+                                        onChange={onShiftNameChange(1004)}
+                                        value={glTable[3].shiftName}
+                                        />
+                                    </th>
                             </tr>
                             <tr className="working-shifts__table-row working-shifts__table-row-team">
-                                <td className="working-shifts__table-cell working-shifts__table-cell-team">Команда 1</td>
-                                <td className="working-shifts__table-cell working-shifts__table-cell-team">Команда 2</td>
-                                <td className="working-shifts__table-cell working-shifts__table-cell-team">Команда 3</td>
-                                <td className="working-shifts__table-cell working-shifts__table-cell-team">-</td>
+                                <td className="working-shifts__table-cell working-shifts__table-cell-team">
+                                    <input
+                                        className="working-shifts__table-header-value"
+                                        type="text"
+                                        onChange={onTeamNameChange(1001)}
+                                        value={glTable[0].teamName}
+                                        />
+                                    </td>
+                                <td className="working-shifts__table-cell working-shifts__table-cell-team">
+                                    <input
+                                        className="working-shifts__table-header-value"
+                                        type="text"
+                                        onChange={onTeamNameChange(1002)}
+                                        value={glTable[1].teamName}
+                                        />
+                                    </td>
+                                <td className="working-shifts__table-cell working-shifts__table-cell-team">
+                                    <input
+                                        className="working-shifts__table-header-value"
+                                        type="text"
+                                        onChange={onTeamNameChange(1003)}
+                                        value={glTable[2].teamName}
+                                        />
+                                    </td>
+                                <td className="working-shifts__table-cell working-shifts__table-cell-team">
+                                    <input
+                                        className="working-shifts__table-header-value"
+                                        type="text"
+                                        onChange={onFirstShiftNumberChange(1004)}
+                                        value={glTable[3].firstShiftNumber}
+                                        />
+                                    </td>
                             </tr> 
                             <tr className="working-shifts__table-row working-shifts__table-row-shifts">
-                                <td className="working-shifts__table-cell working-shifts__table-cell-shift">Смена 1</td>
-                                <td className="working-shifts__table-cell working-shifts__table-cell-shift">Смена 2</td>
-                                <td className="working-shifts__table-cell working-shifts__table-cell-shift">Смена 1 и 2</td>
-                                <td className="working-shifts__table-cell working-shifts__table-cell-shift">-</td>
+                                <td className="working-shifts__table-cell working-shifts__table-cell-shift">
+                                    <input
+                                        className="working-shifts__table-header-value"
+                                        type="text"
+                                        onChange={onShiftNumberChange(1001)}
+                                        value={glTable[0].shiftNumber}
+                                        />
+                                </td>
+                                <td className="working-shifts__table-cell working-shifts__table-cell-shift">
+                                    <input
+                                        className="working-shifts__table-header-value"
+                                        type="text"
+                                        onChange={onShiftNumberChange(1002)}
+                                        value={glTable[1].shiftNumber}
+                                        />
+                                </td>
+                                <td className="working-shifts__table-cell working-shifts__table-cell-shift">
+                                    <input
+                                            className="working-shifts__table-header-value"
+                                            type="text"
+                                            onChange={onShiftNumberChange(1003)}
+                                            value={glTable[2].shiftNumber}
+                                            />
+                                </td>
+                                <td className="working-shifts__table-cell working-shifts__table-cell-shift">
+                                <input
+                                            className="working-shifts__table-header-value"
+                                            type="text"
+                                            onChange={onSecondShiftNumberChange(1004)}
+                                            value={glTable[3].secondShiftNumber}
+                                            />
+                                </td>
                             </tr>
                         </tbody>
                     </table>
-                    {this.tables(7)}
+                    {tables(7)}
                 </div>
                 <div className="working-shifts__table-wrapper">
                     <table className="working-shifts__table">
