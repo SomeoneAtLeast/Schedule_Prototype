6ae70b1e00a9e930ae15364d4d8d53480711ebd6
Вадим Бабичев <WineCooler@yandex.ru>
2021-04-29 20:50:30 +0300

    Тестовая заливка на сервер 0.1

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const config = require("config");
+const path = require("path")
 const mongoose = require("mongoose");
 
 const app = express();
@@ -10,6 +11,14 @@ app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/seats", require("./routes/seats.routes"));
 app.use("/api/shifts", require("./routes/shifts.routes"));
 app.use("/api/workers", require("./routes/workers.routes"));
+
+if (process.env.NODE_ENV === "production") {
+    app.use("/", express.static(path.join(__dirname, "client", "build")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 const PORT = config.get("port") || 5000;
 
